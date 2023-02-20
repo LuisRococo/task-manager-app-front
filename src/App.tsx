eddacbcbb3f1f937eaf69e5d10eb620ace7d687e
Luis Roberto <luisrobertopenav@gmail.com>
2023-02-20 13:06:01 -0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BoardPage from "./pages/BoardPage";
+import { Footer } from "./components/common/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <BoardPage />
+      <Footer />
     </div>
   );
 }
