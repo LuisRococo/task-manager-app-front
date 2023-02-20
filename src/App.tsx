73eab003ac59f4898eab8983b4f7518807711494
Luis Roberto <luisrobertopenav@gmail.com>
2023-02-20 13:14:25 -0700

    add placeholder navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import BoardPage from "./pages/BoardPage";
 import { Footer } from "./components/common/Footer/Footer";
+import { Navigation } from "./components/common/Navigation/Navigation";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Navigation />
       <BoardPage />
       <Footer />
     </div>
---
+++ src/components/common/Navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logoImg from "../../../assets/logo.png";
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav className="navbar bg-dark navbar-expand-lg navbar-dark">
+      <div className="container py-2">
+        <div className="d-flex align-items-center text-white">
+          <img src={logoImg} alt="Project logo" className="logo" />
+          GreatBoard
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav"></ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
